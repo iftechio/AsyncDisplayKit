49478605f6e677f19b89ab3315db762ee33e5d01
Jason Yu <jasy@outlook.com>
2017-02-14 11:42:53 +0800

    fix text tail truncation issue

--- AsyncDisplayKit/TextKit/ASTextKitTailTruncater.mm
+++ AsyncDisplayKit/TextKit/ASTextKitTailTruncater.mm
@@ -151,7 +151,7 @@
     NSUInteger originalStringLength = textStorage.length;
 
     [layoutManager ensureLayoutForTextContainer:textContainer];
-
+    [self adjustTextContainerSize:textContainer byAddingLinespacingOf:textStorage];
     NSRange visibleGlyphRange = [layoutManager glyphRangeForBoundingRect:{ .size = textContainer.size }
                                                          inTextContainer:textContainer];
     NSRange visibleCharacterRange = [layoutManager characterRangeForGlyphRange:visibleGlyphRange
@@ -187,6 +187,26 @@
   }
 
   return NSMakeRange(NSNotFound, 0);
+}
+
+- (void)adjustTextContainerSize:(NSTextContainer *)textContainer
+          byAddingLinespacingOf:(NSTextStorage *)textStorage
+{
+  __block CGFloat lineSpacing = 0;
+  
+  if (textStorage != nil) {
+    [textStorage enumerateAttribute:NSParagraphStyleAttributeName inRange:NSMakeRange(0, textStorage.length) options:0 usingBlock:^(id value, NSRange range, BOOL *stop) {
+      if (value) {
+        NSParagraphStyle *paragraphStyle = (NSParagraphStyle *)value;
+        lineSpacing = paragraphStyle.lineSpacing;
+      }
+    }];
+  }
+  
+  CGFloat width = textContainer.size.width;
+  CGFloat height = textContainer.size.height + lineSpacing;
+  CGSize adjustedSize = CGSizeMake(width, height);
+  textContainer.size = adjustedSize;
 }
 
 @end
