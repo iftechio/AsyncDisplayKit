95c81cb471e0016986ad58581c831fe4f4863ef8
Ballad <348649634@qq.com>
2021-08-26 11:00:43 +0800

    feat: remove ASVideo related

--- Source/ASExperimentalFeatures.h
+++ Source/ASExperimentalFeatures.h
@@ -16,19 +16,17 @@ NS_ASSUME_NONNULL_BEGIN
  * A bit mask of features. Make sure to update configuration.json when you add entries.
  */
 typedef NS_OPTIONS(NSUInteger, ASExperimentalFeatures) {
-  // If AS_ENABLE_TEXTNODE=0 or TextNode2 subspec is used this setting is a no op and ASTextNode2
-  // will be used in all cases
-  ASExperimentalInterfaceStateCoalescing = 1 << 1,                          // exp_interface_state_coalesce
-  ASExperimentalLayerDefaults = 1 << 2,                                     // exp_infer_layer_defaults
-  ASExperimentalCollectionTeardown = 1 << 3,                                // exp_collection_teardown
-  ASExperimentalFramesetterCache = 1 << 4,                                  // exp_framesetter_cache
-  ASExperimentalSkipClearData = 1 << 5,                                     // exp_skip_clear_data
-  ASExperimentalDidEnterPreloadSkipASMLayout = 1 << 6,                      // exp_did_enter_preload_skip_asm_layout
-  ASExperimentalDispatchApply = 1 << 7,                                     // exp_dispatch_apply
-  ASExperimentalDrawingGlobal = 1 << 8,                                     // exp_drawing_global
-  ASExperimentalOptimizeDataControllerPipeline = 1 << 9,                    // exp_optimize_data_controller_pipeline
-  ASExperimentalDisableGlobalTextkitLock = 1 << 10,                         // exp_disable_global_textkit_lock
-  ASExperimentalMainThreadOnlyDataController = 1 << 11,                     // exp_main_thread_only_data_controller
+  ASExperimentalInterfaceStateCoalescing = 1 << 0,                          // exp_interface_state_coalesce
+  ASExperimentalLayerDefaults = 1 << 1,                                     // exp_infer_layer_defaults
+  ASExperimentalCollectionTeardown = 1 << 2,                                // exp_collection_teardown
+  ASExperimentalFramesetterCache = 1 << 3,                                  // exp_framesetter_cache
+  ASExperimentalSkipClearData = 1 << 4,                                     // exp_skip_clear_data
+  ASExperimentalDidEnterPreloadSkipASMLayout = 1 << 5,                      // exp_did_enter_preload_skip_asm_layout
+  ASExperimentalDispatchApply = 1 << 6,                                     // exp_dispatch_apply
+  ASExperimentalDrawingGlobal = 1 << 7,                                     // exp_drawing_global
+  ASExperimentalOptimizeDataControllerPipeline = 1 << 8,                    // exp_optimize_data_controller_pipeline
+  ASExperimentalDisableGlobalTextkitLock = 1 << 9,                         // exp_disable_global_textkit_lock
+  ASExperimentalMainThreadOnlyDataController = 1 << 10,                     // exp_main_thread_only_data_controller
   ASExperimentalFeatureAll = 0xFFFFFFFF
 };
 
